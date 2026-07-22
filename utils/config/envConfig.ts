4e9893c0f6085c99b5a3e0ccee8ad901e05f88c0
MoniruzzamanBillal <mdmoniruzzamanbillal@gmail.com>
2026-07-23 05:52:20 +0600

    live url

--- utils/config/envConfig.ts
+++ utils/config/envConfig.ts
@@ -1,6 +1,10 @@
+//! deployed base url
+export const baseURL = "https://bikelog-server.vercel.app";
+
 // ! main base url
-export const baseURL = "http://localhost:5000";
+// export const baseURL = "http://localhost:5000";
 
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || `${baseURL}/api`;
+  return `${baseURL}/api`;
+  // return process.env.NEXT_PUBLIC_API_BASE_URL || `${baseURL}/api`;
 };
