c8da2203ab19419aa92b08b8881175a8b25c94dc
MoniruzzamanBillal <mdmoniruzzamanbillal@gmail.com>
2026-08-18 16:46:03 +0600

    fix:env config

--- utils/config/envConfig.ts
+++ utils/config/envConfig.ts
@@ -1,3 +1,4 @@
 export const getBaseUrl = (): string => {
-  return `${process.env.NEXT_PUBLIC_API_BASE_URL}/api` as string;
+  return `https://bikelog-server.vercel.app/api` as string;
+  // return `${process.env.NEXT_PUBLIC_API_BASE_URL}/api` as string;
 };
